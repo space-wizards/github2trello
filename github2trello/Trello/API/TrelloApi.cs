6cf2fae0bd4241a59afc65b7ad365a5bc62691b0
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2022-05-11 16:45:49 +0200

    Rename API_KEY and API_TOKEN to TRELLO_API_KEY and TRELLO_API_TOKEN

--- github2trello/Trello/API/TrelloApi.cs
+++ github2trello/Trello/API/TrelloApi.cs
@@ -4,6 +4,6 @@ public static class TrelloApi
 {
     public const string ApiUrl = "https://api.trello.com/1/";
     public static readonly HttpClient Client = new();
-    public static readonly string Key = EnvExtensions.GetOrThrow("API_KEY");
-    public static readonly string Token = EnvExtensions.GetOrThrow("API_TOKEN");
+    public static readonly string Key = EnvExtensions.GetOrThrow("TRELLO_API_KEY");
+    public static readonly string Token = EnvExtensions.GetOrThrow("TRELLO_API_TOKEN");
 }

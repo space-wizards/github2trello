d6c3c5b773d55ed445d55b86b8bbc446d4480a5a
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2022-05-11 16:43:00 +0200

    Add reading parameters from console input

---
+++ github2trello/DateExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace github2trello;
+
+public static class DateExtensions
+{
+    /// <summary>
+    ///     Ensures the day part of a date in the format YYYY-MM-DD is valid
+    /// </summary>
+    /// <param name="date">Date to fix if invalid</param>
+    /// <returns>The original date with the correct number of days if invalid for that month.</returns>
+    public static string FixMonthDays(string? date)
+    {
+        if (date == null)
+        {
+            throw new ArgumentException("No date was given, expected one in format YYYY-MM-DD");
+        }
+        
+        var split = date.Split("-");
+
+        if (split.Length != 3)
+        {
+            throw new ArgumentException($"Invalid date: {date}. Format expected: YYYY-MM-DD");
+        }
+
+        if (!int.TryParse(split[0], out var year))
+        {
+            throw new ArgumentException($"{split[0]} is not a valid numeric year.");
+        }
+        
+        if (!int.TryParse(split[1], out var month))
+        {
+            throw new ArgumentException($"{split[1]} is not a valid numeric month");
+        }
+
+        if (!int.TryParse(split[2], out var day))
+        {
+            throw new ArgumentException($"{split[2]} is not a valid numeric day");
+        }
+
+        var daysInMonth = DateTime.DaysInMonth(year, month);
+        return daysInMonth < day ? $"{year}-{month}-{daysInMonth}" : date;
+    }
+}
--- github2trello/Program.cs
+++ github2trello/Program.cs
@@ -1,14 +1,24 @@
-﻿using github2trello.GitHub;
+﻿using github2trello;
+using github2trello.GitHub;
 using github2trello.Trello.API;
 using Octokit;
 
+Console.WriteLine("Input a repo owner. Example: space-wizards");
+var repoOwner = Console.ReadLine() ?? throw new NullReferenceException();
+
+Console.WriteLine("Input a repo name. Example: space-station-14");
+var repoName = Console.ReadLine() ?? throw new NullReferenceException();
+
+Console.WriteLine("Input a starting date (YYYY-MM-DD). Example: 2022-04-01");
+var createdAfter = DateExtensions.FixMonthDays(Console.ReadLine());
+
+Console.WriteLine("Input an ending date (YYYY-MM-DD). Example: 2022-04-30");
+var createdBefore = DateExtensions.FixMonthDays(Console.ReadLine());
+
 await GitHubApi.Login();
 
-const string createdAfter = "2022-04-01";
-const string createdBefore = "2022-04-30";
-
 var prs = new List<Issue>(500);
-prs.AddRange(await GitHubSearch.PRsMerged("space-wizards", "space-station-14", createdAfter, createdBefore));
+prs.AddRange(await GitHubSearch.PRsMerged(repoOwner, repoName, createdAfter, createdBefore));
 var list = await TrelloLists.Create("Imported PRs (Space Station 14)", args[0]);
 
 var cards = new List<Func<ValueTask>>();
